be2eea64f93ab3dc74e648e984791f1b337d6fd1
andres hernandez <andress.hernandezp@ecci.edu.co>
2022-06-10 20:27:19 -0500

    configuracion de heroku

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 const publicPath = path.resolve(__dirname, "./public")
 app.use(express.static(publicPath));
 
-app.listen(3000,() => {
+app.listen(process.env.PORT 3000,() => {
 console.log("Servidor corriendo en el puerto 3000");
 });
 
